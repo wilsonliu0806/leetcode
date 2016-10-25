de4ac0543aa45e36fc97bcfa9747f590b89bf9b3
wilsonliu0806@gmail.com <wilsonliu0806@gmail.com>
2016-10-25 17:19:15 +0800

    wrong

--- lengthOfLongestSubstring.c
+++ lengthOfLongestSubstring.c
@@ -19,6 +19,7 @@ int lengthOfLongestSubstring(char* s) {
     int end[MAX_STRING]={0};
     int start[MAX_STRING]={0};
     int repeat[MAX_STRING]={0};
+    if(s == NULL || *s=='\0') return 0;
     while(*(s+i-1)!='\0'){
 	printf("%d ",i);
         if(i == 1){
@@ -30,30 +31,35 @@ int lengthOfLongestSubstring(char* s) {
             continue;
         }
         
+        int repeatpos = -1;
         if(end[i-1] == i-1){
-            startpos=start[i-1];
-            endpos = end[i-1];
-        }else if(i-repeat[i-1]>=end[i-1]-start[i-1]+1){
-            startpos=i-(end[i-1]-start[i-1]);
-            endpos=i-1;
-        }else{
-            start[i]=start[i-1];
-            end[i]=end[i-1];
-            i++;
-            continue;
+            repeatpos = sub_len_has(s,start[i-1],end[i-1],*(s+i-1));
+            if(repeatpos == 0){
+                start[i]=start[i-1];
+                end[i]=i;
+                i++;
+                continue;
+            }
         }
-        if((repeat[i]=sub_len_has(s,startpos,endpos,*(s+i-1)))== 0){
-            start[i]=startpos;
-            end[i]=i;
-        }else{
-            start[i]=start[i-1];
-            end[i]=end[i-1];
+
+        if(i-repeat[i-1]>=end[i-1]-start[i-1]+1){
+            repeatpos = sub_len_has(s,i-(end[i-1]-start[i-1]),i-1,*(s+i-1));
+            if(i-repeatpos >= end[i-1]-start[i-1]+1)
+                start[i]=i-(end[i-1]-start[i-1]);
+                end[i]=i;
+                i++;
+                continue;
+            }
         }
+
+        start[i]=start[i-1];
+        end[i]=end[i-1];
+
         printf("%d %d %d ",start[i],end[i],repeat[i]);
         i++;
         printf("\n");
     }
-    return end[i]-start[i]+1;
+    return end[i-1]-start[i-1]+1;
 }
 int main()
 {
