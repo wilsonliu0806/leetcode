fc539b68649d15f93f08899f01c5c53cf70e97aa
wilsonliu0806@gmail.com <wilsonliu0806@gmail.com>
2016-10-25 15:30:18 +0800

    version 2

--- lengthOfLongestSubstring.c
+++ lengthOfLongestSubstring.c
@@ -1,10 +1,17 @@
 #include"stdio.h"
 #define MAX_STRING 256
-int is_valid_substr(char *s,int start,int end){
-
-}
 int sub_len_has(char * s,int start,int end,char ch){
-    
+    char *p = s+start;
+    while(p!='\0'){
+        if(*p == ch){
+            return p-s+1;
+        }
+        if(p - s == end){
+            break;
+        }
+        p++;
+    }    
+    return 0;
 }
 int lengthOfLongestSubstring(char* s) {
     int i=1;
@@ -23,24 +30,20 @@ int lengthOfLongestSubstring(char* s) {
         if(end[i-1] == i-1){
             startpos=start[i-1];
             endpos = end[i-1];
-        }else{
+        }else if(i-repeat[i-1]>=end[i-1]-start[i-1]+1){
             startpos=repeat[i-1]+1;
             endpos=i-1;
-        }
-        if(repeat[i]=sub_len_has(s,start[i-1],end[i-1],s+i)){
-            start[i]=start[i-1];
-            end[i]=end[i-1];
         }else{
             start[i]=start[i-1];
-            end[i]=i;
+            end[i]=end[i-1];
+            continue;
         }
-            if(repeat[i]=sub_len_has(s,repeat[i-1]+1,i-1,s+i)){
-                start[i]=start[i-1];
-                end[i]=end[i-1];
-            }else{
-                start[i]=repeat[i-1]+1;
-                end[i]=i;
-            }
+        if(!repeat[i]=sub_len_has(s,startpos,endpos,s+i)){
+            start[i]=startpos;
+            end[i]=i;
+        }else{
+            start[i]=start[i-1];
+            end[i]=end[i-1];
         }
         i++;
     }
