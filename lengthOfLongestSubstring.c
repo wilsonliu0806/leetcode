cc548209957961682ed46c5b00af572414629294
wilsonliu0806@gmail.com <wilsonliu0806@gmail.com>
2016-10-25 16:25:43 +0800

    correct version

--- lengthOfLongestSubstring.c
+++ lengthOfLongestSubstring.c
@@ -1,12 +1,12 @@
 #include"stdio.h"
 #define MAX_STRING 256
 int sub_len_has(char * s,int start,int end,char ch){
-    char *p = s+start;
+    char *p = s+start-1;
     while(p!='\0'){
         if(*p == ch){
             return p-s+1;
         }
-        if(p - s == end){
+        if(p - s == end-1){
             break;
         }
         p++;
@@ -16,15 +16,17 @@ int sub_len_has(char * s,int start,int end,char ch){
 int lengthOfLongestSubstring(char* s) {
     int i=1;
     int startpos,endpos;
-    int end[MAX_STRING];
-    int start[MAX_STRING];
-    int repeat[MAX_STRING];
+    int end[MAX_STRING]={0};
+    int start[MAX_STRING]={0};
+    int repeat[MAX_STRING]={0};
     while(*(s+i-1)!='\0'){
+	printf("%d ",i);
         if(i == 1){
             start[i]=1;
             end[i]=1;
             repeat[i]=0;
             i++;
+	printf("\n");
             continue;
         }
         
@@ -32,7 +34,7 @@ int lengthOfLongestSubstring(char* s) {
             startpos=start[i-1];
             endpos = end[i-1];
         }else if(i-repeat[i-1]>=end[i-1]-start[i-1]+1){
-            startpos=repeat[i-1]+1;
+            startpos=i-(end[i-1]-start[i-1]);
             endpos=i-1;
         }else{
             start[i]=start[i-1];
@@ -40,14 +42,16 @@ int lengthOfLongestSubstring(char* s) {
             i++;
             continue;
         }
-        if(repeat[i]=sub_len_has(s,startpos,endpos,*(s+i)) == 0){
+        if((repeat[i]=sub_len_has(s,startpos,endpos,*(s+i-1)))== 0){
             start[i]=startpos;
             end[i]=i;
         }else{
             start[i]=start[i-1];
             end[i]=end[i-1];
         }
+        printf("%d %d %d ",start[i],end[i],repeat[i]);
         i++;
+        printf("\n");
     }
     return end[i]-start[i]+1;
 }
